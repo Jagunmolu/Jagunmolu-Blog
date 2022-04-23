ab72b98ef9f6aa85713705e033ec6c52e738e997
Jagunmolu <kolawolejonathan@gmail.com>
2022-04-23 11:06:31 +0100

    added create, update and delete functions

--- blog/tests.py
+++ blog/tests.py
@@ -42,3 +42,26 @@ class BlogTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'A good title')
         self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'), {
+            'title': 'Post 5',
+            'body': 'This is the fifth post.',
+            'author': self.user
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Post 5')
+        self.assertContains(response, 'This is the fifth post.')
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': '(Updated) Post 5',
+            'body': '(Updated) This is the fifth post.',
+        })
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response = self.client.get(
+            reverse('post_delete', args='1')
+        )
+        self.assertEqual(response.status_code, 200)
