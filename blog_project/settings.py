ad6c025c63338c42f6573a36bed7f189ec465781
Jagunmolu <kolawolejonathan@gmail.com>
2022-04-24 05:55:41 +0100

    set debug to false

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mq$w*)viogn#t$ih$r$@@u$3jyxu5&nm571pxhd$1jahx59g)p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
